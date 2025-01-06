f10b8aca4a92235f7e949df73c536ca44f164f38
Vignesh-Poonja <146766203+Vignesh-Poonja@users.noreply.github.com>
2025-01-06 09:49:16 +0000

    commit 1

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const stripe = require("stripe")(api_key);
 
 // Setting up the static folder:
 // app.use(express.static(resolve(__dirname, "./client")));
-app.use(express.static(resolve(__dirname, process.env.STATIC_DIR)));
+const staticDir = process.env.STATIC_DIR || './client';
+app.use(express.static(resolve(__dirname, staticDir)));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
